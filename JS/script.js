f7e5a16b040a2b990c632f0c1db6ff3b45470187
Tommy007U <tozakitoko2@gmail.com>
2023-05-29 12:31:30 +0200

    working on reset button and random color button

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,25 @@
 let color = "black";
+let click = false;
 
 // method below is to allow HTML & CSS to load 1st bfr JS load help avoid the page to crash in sm cases
 document.addEventListener("DOMContentLoaded", function(){
     //console.log("Hi!!");
     createGrid(16);
+
+    // once click on the body the event trigger a message to draw or not to
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click)
+                draw.innerHTML="You are allowed to draw now";
+                draw.style.color= "blue";
+            }else{
+                draw.innerHTML = "You are not yet allowed to draw Click again!";
+                draw.style.color = "red";
+            }
+        });
+
     // this var will triger the button to influence the f(x) to create nbr of cells the user type on the prompt
     let popup_btn = document.querySelector("#popup");
     popup_btn.addEventListener("click", function(){
@@ -24,13 +40,13 @@ function createGrid(cells){
     let totalDivs = cells * cells;
     // for loop to iterate thru the creation of expected total amount of div
     for(let i = 0; i < totalDivs; i++){
-        let newDivs = document.createElement("div");
+        let div = document.createElement("div");
         // event to start applying color on the cells
-        newDivs.addEventListener("mouseover", colorDiv)
-            newDivs.style.backgroundColor="red";
+        div.addEventListener("mouseover", colorDiv)
+            div.style.backgroundColor="red";
         
-        newDivs.style.backgroundColor= "lightgray";
-        main_div.insertAdjacentElement("beforeend", newDivs);
+        div.style.backgroundColor= "lightgray";
+        main_div.insertAdjacentElement("beforeend", div);
     }
 
 }
@@ -42,25 +58,30 @@ function selectSize(){
         message.innerHTML = "Please Provide a Number ";
         
     }else if(input < 0 || input > 100){
-        message.innerHTML =" Please Provide  Number between 0 to 100";
+        message.innerHTML =" Please Provide  Number between 1 to 100";
     }else{
         message.innerHTML = " Now you can Play!!!";
         return input;
     }
 }
-function colorDiv(){
-    if(color == "random"){
-        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
-    }else{
+function colorDiv(){  
+    if(click){ 
+        
+       if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)` // this represent the div
+       }else{
         this.style.backgroundColor ="black";
-    }
+      }
+    } 
 
 }
 
 function setColor(colorChoice){
     color = colorChoice;
 }
+
+// f(x) once click it will reset all color to white by using forEach() method  
 function resetCells(){
-    let resetDivs = document.querySelectorAll("newDivs");
-    resetDivs.forEach((newDivs) =>newDivs.style.backgroundColor="white");
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor= "white");
 }
